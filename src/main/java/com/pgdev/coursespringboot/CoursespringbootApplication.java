f4e9e899a3c0ed0fbe730fe463ec6a59e3b9588a
P3dr0S17v5 <pedro.graffic@gmail.com>
2023-09-11 12:11:15 +0100

    Initial commit

---
+++ src/main/java/com/pgdev/coursespringboot/CoursespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pgdev.coursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringbootApplication.class, args);
+	}
+
+}
