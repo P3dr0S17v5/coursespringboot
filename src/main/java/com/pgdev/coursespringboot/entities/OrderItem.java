bb1c9d70c0b24c91e49df3d875c41fc1a8ad608e
P3dr0S17v5 <pedro.graffic@gmail.com>
2023-09-14 11:32:02 +0100

    Product-OrderItem one-to-many association

--- src/main/java/com/pgdev/coursespringboot/entities/OrderItem.java
+++ src/main/java/com/pgdev/coursespringboot/entities/OrderItem.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.pgdev.coursespringboot.entities.pk.OrderItemPK;
 
 import jakarta.persistence.EmbeddedId;
